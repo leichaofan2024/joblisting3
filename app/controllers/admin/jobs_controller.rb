c91537dba75cc81e7955d04673b43c4037a4f71a
leichaofan <leichaofan2024@gmail.com>
2017-03-12 23:47:25 +0800

    generate admin/job controller and complete write the page of controller

---
+++ app/controllers/admin/jobs_controller.rb
@@ -0,0 +1,43 @@
+class Admin::JobsController < ApplicationController
+  before_action :authenticate_user!, only:[:new,:create,:edit,:update,:destroy]
+  def index
+    @jobs = Job.all
+  end
+  def show
+    @job = Job.find(params[:id])
+  end
+  def new
+    @job = Job.new
+  end
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to admin_jobs_path, notice: "创建成功！"
+    else
+      render :new
+    end
+  end
+  def edit
+    @job = Job.find(params[:id])
+  end
+  def update
+    @job = Job.find(params[:id])
+
+    if @job.update(job_params)
+     redirect_to admin_jobs_path, notice: "update success!"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to admin_jobs_path, warning:"成功删除！"
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end 
+end
