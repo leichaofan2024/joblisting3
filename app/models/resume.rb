e904175a977ed1f8e68f1b4c475cf9283ab321f0
leichaofan <leichaofan2024@gmail.com>
2017-03-13 21:34:55 +0800

    fix bug

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -3,5 +3,6 @@ class Resume < ApplicationRecord
   belongs_to :user
   validates :content, presence: true
   mount_uploader :attachment, AttachmentUploader
-  
+
+  scope :recent, -> {order("created_at DESC")}
 end
