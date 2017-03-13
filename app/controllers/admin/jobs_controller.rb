47767f73c45bb97b932f3b45e7b2e32cbd5d9c2d
leichaofan <leichaofan2024@gmail.com>
2017-03-13 09:20:15 +0800

    add four page of views and judgement of admin“ ” “

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only:[:new,:create,:edit,:update,:destroy]
+  before_action :require_is_admin
   def index
     @jobs = Job.all
   end
@@ -34,10 +35,14 @@ class Admin::JobsController < ApplicationController
     @job.destroy
     redirect_to admin_jobs_path, warning:"成功删除！"
   end
-
+  def require_is_admin
+    if !current_user.admin?
+      redirect_to jobs_path, alert:  "你不是管理员啊！"
+    end
+  end
   private
 
   def job_params
     params.require(:job).permit(:title, :description)
-  end 
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  def admin?
+    email == "leichaofan2024@gmail.com"
+  end 
 end
